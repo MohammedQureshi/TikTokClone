580f51cee1e7e2cd1056782490a5674c1a2228ec
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-10 21:54:20 +0100

    initial commit of the sidenav react componentwith comments as we'll building up the component slowly.

---
+++ src/components/sidenav/SideNav.tsx
@@ -0,0 +1,13 @@
+
+
+export const SideNav : React.FC = () => {
+    // nav list icon & name
+    // paragrah text 
+    // button 
+    // header 
+    // suggested accounts
+    // header 
+    // hashtags (discover)
+    // nav list 
+    return <></>
+}
