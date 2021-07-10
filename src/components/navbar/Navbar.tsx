2006eda975c40ae449c60e22e85e69c21fbc96bc
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-10 23:18:12 +0100

    updated to use ButtonType instead of strings :)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '../button/Button'
+import Button, { ButtonType } from '../button/Button'
 import SearchBox from '../searchbox/SearchBox'
 import './Navbar.scss'
 import TikTokLogo from '../../images/TikTokLogo.svg'
@@ -14,8 +14,8 @@ function Navbar(){
         <div className="navbar">
             <img src={TikTokLogo} alt="TikTok Logo"/>
             <SearchBox/>
-            <Button type="normal" text="Upload" event={doSomething}/>
-            <Button type="filled" text="Login" event={doSomething}/>
+            <Button type={ButtonType.NORMAL} text="Upload" event={doSomething}/>
+            <Button type={ButtonType.FILLED} text="Login" event={doSomething}/>
             <button className="ellipsis"><i className="fas fa-ellipsis-v"></i></button>
         </div>
     )
