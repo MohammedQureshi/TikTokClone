139b2bd8c765189a3076c35901d2841c1289252c
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-10 21:15:48 +0100

    Add support for font awesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import './App.css';
 import Home from './pages/home/Home'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar(){
+    return(
+        <>
+            <i className="fas fa-space-shuttle" />
+        </>
+    )
+}
+
+export default Navbar
