5d7967e0497614772c377b4942b6ac1b0f1c988b
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-10 21:54:53 +0100

    added navlist react component to the home.tsx file

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { NavList } from '../../components/navlist/NavList'
+import NavListItem from '../../components/navlist/NavListItem';
+import { FontAwesome } from '../../shared/icons/icon';
 
 function Home() {
+    
+    const navItems : Array<JSX.Element> = [
+        <NavListItem icon={<i className={FontAwesome.Icon.HOME}/>} text={"For You"}/>,
+        <NavListItem icon={<i className={FontAwesome.Icon.FOLLOWING}/>} text={"Following"}/>
+    ];
     return(
         <div>
+            <NavList items={navItems}/>
         </div>
     )
 }
