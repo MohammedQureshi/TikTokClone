ddfaaa30222f47ea18914c1525d9398672c264a0
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-10 20:52:40 +0100

    Fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/Home'
 
-import { Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
