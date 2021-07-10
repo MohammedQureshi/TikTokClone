1a8894dc95a8cf94768ceb5c3bc8309943d538a9
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-07-10 23:09:20 +0100

    Add components to nav bar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../button/Button'
 import SearchBox from '../searchbox/SearchBox'
 import './Navbar.scss'
+import TikTokLogo from '../../images/TikTokLogo.svg'
 
 function Navbar(){
 
@@ -11,10 +12,11 @@ function Navbar(){
     }
     return(
         <div className="navbar">
+            <img src={TikTokLogo} alt="TikTok Logo"/>
             <SearchBox/>
             <Button type="normal" text="Upload" event={doSomething}/>
             <Button type="filled" text="Login" event={doSomething}/>
-            <Button type="outline" text="Login" event={doSomething}/>
+            <button className="ellipsis"><i className="fas fa-ellipsis-v"></i></button>
         </div>
     )
 }
