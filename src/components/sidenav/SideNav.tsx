499b4fe7e03228ba0acabebc79551193c8a97e01
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-14 00:03:32 +0100

    updated to include the new NavList.Nav with the NavList items, span, p and button.

--- src/components/sidenav/SideNav.tsx
+++ src/components/sidenav/SideNav.tsx
@@ -1,4 +1,8 @@
-
+import React from "react";
+import { FontAwesome } from "../../shared/icons/icon";
+import Button, { ButtonType } from "../button/Button";
+import NavList from "../navlist/NavList";
+import './SideNav.scss';
 
 export const SideNav : React.FC = () => {
     // nav list icon & name
@@ -9,5 +13,13 @@ export const SideNav : React.FC = () => {
     // header 
     // hashtags (discover)
     // nav list 
-    return <></>
+    return (<React.Fragment>
+        <NavList.Nav>
+            <NavList.Item icon={<i className={FontAwesome.Icon.HOME}/>} text={"For You"} className={"selected"}/>
+            <NavList.Item icon={<i className={FontAwesome.Icon.FOLLOWING}/>} text={"Following"}/>
+        </NavList.Nav>
+        <span className={"nav-wrapper"}></span>
+        <p>Log in to follow creators, like videos, and view comments.</p>
+        <Button type={ButtonType.PINK_OUTLINED} text={"Log in"}/>
+    </React.Fragment>)
 }
