48099cb9bdd2784271abfd2a4394a5272e31bfa2
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-14 00:00:58 +0100

    updated to include new SideNav element.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import { NavList } from '../../components/navlist/NavList'
-import NavListItem from '../../components/navlist/NavListItem';
+import { SideNav } from '../../components/sidenav/SideNav';
 import { FontAwesome } from '../../shared/icons/icon';
 import './Home.scss';
 
 import Post from '../../components/post/Post';
 
 function Home() {
-    
-    const navItems : Array<JSX.Element> = [
-        <NavListItem icon={<i className={FontAwesome.Icon.HOME}/>} text={"For You"} className={"selected"}/>,
-        <NavListItem icon={<i className={FontAwesome.Icon.FOLLOWING}/>} text={"Following"}/>
-    ];
     return(
         <div className="main-body">
-            <div className="side-navbar"><NavList items={navItems}/></div>
+            <div className="side-navbar">
+                <div className="nav-container">
+                    <SideNav/>
+                </div>
+            </div>
             <div className="post-feed">
                 <Post/>
             </div>
