6f7a7147dee22a948a35f130ee77047b678f6d0e
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-14 00:05:21 +0100

    updated to include NavList.Nav and NavList.Item under the same tsx class which is better for readability.

--- src/components/navlist/NavList.tsx
+++ src/components/navlist/NavList.tsx
@@ -1,14 +1,38 @@
 import { ReactElement } from "react";
-import NavListItem from "./NavListItem";
+import { SideNav } from "../sidenav/SideNav";
 import './NavList.scss';
 
-interface NavListProps {
-    items?: Array<ReactElement<JSX.Element>>
+interface NavListItemProps {
+    icon?: ReactElement<JSX.IntrinsicElements>,
+    text?: string,
+    className?: string
 }
-export const NavList = ({
-    items = new Array<JSX.Element>()
+
+const Item : React.FC<NavListItemProps> = ({
+    icon = null,
+    text = "",
+    className = undefined
+}) => { return <li className={className}>{icon}{text}</li>}    
+
+
+interface NavListProps {
+    className?: string,
+    children?: Array<ReactElement<typeof Item>>
+}
+
+const Nav : React.FC<NavListProps> = ({
+    className = undefined,
+    children = new Array<ReactElement<typeof Item>>()
 }) => {
-    return <ul className={"navlist"}>
-        { items.map((item: JSX.Element) => item) }
+    const classNameString = className ? `navlist ${className}` : 'navlist';
+    return <ul className={classNameString}>
+        { children.map((item: JSX.Element) => item) }
     </ul>
 }
+
+export const NavList = {
+    Nav,
+    Item
+}
+
+export default NavList;
