26f423d73cbd04457f53755e3d9d89d951b70905
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-15 16:45:02 +0100

    a user list component that that is used within the sidenav. it allows you create account components which contain the avatar image (with alt), user info: title, verfied and description.

---
+++ src/components/userlist/UserList.tsx
@@ -0,0 +1,54 @@
+import React, { ReactElement } from "react";
+import './UserList.scss';
+
+interface AccountProps {
+    username: string,
+    description: string,
+    imageURL: string
+    isVerifed?: boolean
+}
+
+const Account : React.FC<AccountProps> = ({
+    username = "",
+    description = "",
+    imageURL = "",
+    isVerifed = false
+}) : ReactElement => {
+
+    const displayVerifed = !isVerifed ? null :  <i className="fas fa-check-circle"></i>;
+    return <a className="account">
+        <span className="user-item-inner">
+            <div className="user-avatar">
+                <span className="tiktok-avatar">
+                    <img alt={description + " TikTok"} src={imageURL}/>
+                </span>
+            </div>
+            <div className="user-info">
+                <p className="user-title">
+                    <h4>{username}</h4>
+                    { displayVerifed }
+                </p>
+                <p className="user-description"> {description}</p>
+            </div>
+        </span>
+    </a>
+}
+
+interface UserListContainerProps {
+    children: Array<ReactElement<typeof Account>>
+}
+
+const Container : React.FC<UserListContainerProps> = ({
+    children = Array()
+}) => {
+    return <div className="user-list">
+        { children.map(child => child) }
+    </div>
+}
+
+export const UserList = {
+    Container,
+    Account
+}
+
+export default UserList;
