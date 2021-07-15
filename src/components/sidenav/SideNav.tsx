4353b3f52fdf5e32bccd5ea19f65741eab23cad6
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-07-15 16:43:20 +0100

    updated to include the newly created user list which now contains two people Gordon ramsay and Dua Lipa who are both verified!

--- src/components/sidenav/SideNav.tsx
+++ src/components/sidenav/SideNav.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesome } from "../../shared/icons/icon";
 import Button, { ButtonType } from "../button/Button";
 import NavList from "../navlist/NavList";
+import UserList from "../userlist/UserList";
 import './SideNav.scss';
 
 export const SideNav : React.FC = () => {
@@ -21,5 +22,11 @@ export const SideNav : React.FC = () => {
         <span className={"nav-wrapper"}></span>
         <p>Log in to follow creators, like videos, and view comments.</p>
         <Button type={ButtonType.PINK_OUTLINED} text={"Log in"}/>
+        <span className={"nav-wrapper"}></span>
+        <p>Suggested accounts</p>
+        <UserList.Container>
+            <UserList.Account username={"gordonramsayofficial"} description={"Gordon Ramsay"} imageURL={"https://p77-sign-va.tiktokcdn.com/musically-maliva-obj/1647754017437702~c5_100x100.jpeg?x-expires=1626444000&x-signature=COz7D77UKsgRuAa0VCrCckX5ahc%3D"} isVerifed={true}/>
+            <UserList.Account username={"dualipaofficial"} description={"Dua Lipa"} imageURL={"https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1657268694828037~c5_100x100.jpeg?x-expires=1626444000&x-signature=O0GGjCBPfClH49BlsMbUb0vhm7k%3D"} isVerifed={true}/>
+        </UserList.Container>
     </React.Fragment>)
 }
