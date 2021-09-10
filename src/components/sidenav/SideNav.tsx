311de6147078fbc1372aa4d63dbc1f91364dbe8b
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-09-10 19:36:37 +0100

    Download profile pictures and videos for demo

--- src/components/sidenav/SideNav.tsx
+++ src/components/sidenav/SideNav.tsx
@@ -5,6 +5,12 @@ import NavList from "../navlist/NavList";
 import UserList from "../userlist/UserList";
 import './SideNav.scss';
 
+import therock from '../../images/profiles/therock.jpeg';
+import nialhoran from '../../images/profiles/nialhoran.jpeg';
+import coldplay from '../../images/profiles/coldplay.jpeg';
+import gordon from '../../images/profiles/gordon.jpeg'
+import dualipa from '../../images/profiles/dualipa.jpeg'
+
 export const SideNav : React.FC = () => {
     // nav list icon & name
     // paragrah text 
@@ -27,11 +33,11 @@ export const SideNav : React.FC = () => {
         <span className={"nav-wrapper"}></span>
         <p>Suggested accounts</p>
         <UserList.Container>
-            <UserList.Account username={"gordonramsayofficial"} description={"Gordon Ramsay"} imageURL={"https://p77-sign-va.tiktokcdn.com/musically-maliva-obj/1647754017437702~c5_100x100.jpeg?x-expires=1626444000&x-signature=COz7D77UKsgRuAa0VCrCckX5ahc%3D"} isVerifed={true}/>
-            <UserList.Account username={"dualipaofficial"} description={"Dua Lipa"} imageURL={"https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1657268694828037~c5_100x100.jpeg?x-expires=1626444000&x-signature=O0GGjCBPfClH49BlsMbUb0vhm7k%3D"} isVerifed={true}/>
-            <UserList.Account username={"therock"} description={"The Rock"} imageURL={"https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1647596478025734~c5_100x100.jpeg?x-expires=1626462000&x-signature=egvJidY2WQ%2FT0nxg%2BRZvVdXNT24%3D"} isVerifed={true}/>
-            <UserList.Account username={"niallhoran"} description={"Niall Horan"} imageURL={"https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f2933b36608c12c5e36ee5c7906d3a0a~c5_100x100.jpeg?x-expires=1626462000&x-signature=WFZc%2BK2VEAR7qVnuUytJfstWEsg%3D"} isVerifed={true}/>
-            <UserList.Account username={"coldplay"} description={"coldplay"} imageURL={"https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/ac23e6ac9322801426d0b0756bed5bc6~c5_100x100.jpeg?x-expires=1626462000&x-signature=75Tgcz9mp4NBEEUBnW50rhiK%2FqQ%3D"} isVerifed={true}/>
+            <UserList.Account username={"gordonramsayofficial"} description={"Gordon Ramsay"} imageURL={gordon} isVerifed={true}/>
+            <UserList.Account username={"dualipaofficial"} description={"Dua Lipa"} imageURL={dualipa} isVerifed={true}/>
+            <UserList.Account username={"therock"} description={"The Rock"} imageURL={therock} isVerifed={true}/>
+            <UserList.Account username={"niallhoran"} description={"Niall Horan"} imageURL={nialhoran} isVerifed={true}/>
+            <UserList.Account username={"coldplay"} description={"coldplay"} imageURL={coldplay} isVerifed={true}/>
         </UserList.Container>
     </React.Fragment>)
 }
